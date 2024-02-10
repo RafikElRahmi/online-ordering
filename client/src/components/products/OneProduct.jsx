dd37f5641e34f75a0efa511d071257ce46e80d23
ELRAHMI Rafik <rafikrahmi11@gmail.com>
2024-02-10 17:06:54 +0100

    Card product design

--- client/src/components/products/OneProduct.jsx
+++ client/src/components/products/OneProduct.jsx
@@ -1,33 +1,41 @@
-import React from 'react'
-import CartButton from '../cart/cartButton';
-import { useAuth } from '../../context/useAuth';
-import { Button, Card } from 'react-bootstrap';
+import React from "react";
+import CartButton from "../cart/cartButton";
+import { useAuth } from "../../context/useAuth";
+import { Button, Card } from "react-bootstrap";
 
-function OneProduct({ product,update,ondelete }) {
-  const { isAdmin, setitems } = useAuth();
+function OneProduct({ product, update, ondelete }) {
+    const { isAdmin, setitems } = useAuth();
 
-  return (
-    <Card style={{ height: "100%" }}>
-      <Card.Body>
-        <Card.Title>{product.name}</Card.Title>
-        <Card.Text className="pos-end">
-          <span>{product.price}DT</span>
-        </Card.Text>
-        {isAdmin ? (
-          <div className="button-container">
-            <Button variant="danger" onClick={ondelete}>
-              Delete
-            </Button>
-            <Button variant="primary" onClick={update}>
-              Update
-            </Button>
-          </div>
-        ) : (
-          <CartButton product={product} items={setitems} />
-        )}
-      </Card.Body>
-    </Card>
-  );
+    return (
+        <Card style={{ height: "100%" }}>
+            <Card.Body>
+                <Card.Title>{product.name}</Card.Title>
+            </Card.Body>
+            <Card.Footer
+                className="border-top-0"
+                style={{ backgroundColor: "white" }}>
+                <Card.Text className="pos-end fs-5 fw-bold">
+                    <span>{product.price}DT</span>
+                </Card.Text>
+                {isAdmin ? (
+                    <div className="button-container">
+                        <Button variant="danger" onClick={ondelete}>
+                            Delete
+                        </Button>
+                        <Button variant="primary" onClick={update}>
+                            Update
+                        </Button>
+                    </div>
+                ) : (
+                    <CartButton
+                        product={product}
+                        items={setitems}
+                        style={{ bottom: "0" }}
+                    />
+                )}
+            </Card.Footer>
+        </Card>
+    );
 }
 
-export default OneProduct
+export default OneProduct;
