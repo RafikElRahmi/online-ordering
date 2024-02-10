8eac728d9b076226ec0232c35519cb3bc10fc93b
ELRAHMI Rafik <rafikrahmi11@gmail.com>
2024-02-10 13:40:40 +0100

    to vercel

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,5 @@ import './index.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
 )
--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -6,11 +6,11 @@ import Cart from "../pages/cart";
 import Orders from "../pages/orders";
 import PageNotFound from "../pages/page404";
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "login", element: <Login /> },
-  { path: "register", element: <Register /> },
-  { path: "cart", element: <Cart /> },
-  { path: "orders", element: <Orders /> },//heheheh
-  { path: "*", element: <PageNotFound/> },
+    { path: "/", element: <Home /> },
+    { path: "/login", element: <Login /> },
+    { path: "/register", element: <Register /> },
+    { path: "/cart", element: <Cart /> },
+    { path: "/orders", element: <Orders /> },
+    { path: "*", element: <PageNotFound /> },
 ]);
 export default router;
