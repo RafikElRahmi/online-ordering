83207b3dd26bbe7c276c9fb3e2c27bee75c6f055
ELRAHMI Rafik <rafikrahmi11@gmail.com>
2024-02-10 14:08:26 +0100

    to vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "./",
     server: {
         middlewareMode: "index.html",
     },
